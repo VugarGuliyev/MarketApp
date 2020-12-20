94e9f938bf5a512f5f1b4750c1b193edc67c6fa8
VugarGuliyev <vugarmg@code.edu.az>
2020-12-20 16:04:18 +0400

    Sale Class Created

---
+++ Sale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp
+{
+    class Sale
+    {
+        private static int _id = 0;
+        public readonly int ID;
+        public readonly DateTime Time;
+        public double TotalAmount;
+
+        List<SaleItem> saleItems = new List<SaleItem>();
+
+        public Sale()
+        {
+            ID = ++_id;
+            Time = DateTime.Now;
+            TotalAmount = 0;
+        }
+    }
+}
