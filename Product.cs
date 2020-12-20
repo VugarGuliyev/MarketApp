6cc67286367b7febf0ded937f5348073bd29046c
VugarGuliyev <vugarmg@code.edu.az>
2020-12-21 02:19:21 +0400

    Product Class Updated

--- Product.cs
+++ Product.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace MarketApp
 {
-    public enum Category { Shirniyyat, Et_Mehsullari, Sud_Mehsullari, Un_Memulatlari, Meishet_Avadanligi, Ichki, Meyve_Terevez }
+    public enum Category { Shirniyyat = 1, Et_Mehsullari, Sud_Mehsullari, Un_Memulatlari, Meishet_Avadanligi, Ichki, Meyve_Terevez }
     
     class Product
     {
         public string Name { get; set; }
         public double Price { get; set; }
-        public int Count { get; set; }
-        public int Code { get; }
+        public ushort Count { get; set; }
+        public string Code { get; set; }
         public Category Category { get; set; }
 
-        public Product(string name, double price, int count, int code, Category category)
+        public Product()
         {
-            Name = name;
-            Price = price;
-            Count = count;
-            Code = code;
-            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"Mehsul: {Name} | Qiymet: {Price} | Say: {Count} | Kod: {Code} | Kateqoriya: {Category}";
         }
     }
 }
