353428fecfb7d0ba410bda07984d4e368afaf275
VugarGuliyev <vugarmg@code.edu.az>
2020-12-20 15:23:40 +0400

    SaleItem Class Created

---
+++ SaleItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp
+{
+    class SaleItem
+    {
+        private static int _itemIndex = 0;
+        public readonly int ItemIndex;
+        public Product Product { get; set; }
+        public int Count { get; set; }
+
+        public SaleItem()
+        {
+            ItemIndex = ++_itemIndex;
+        }
+
+        public SaleItem(Product product, int count) : this()
+        {
+            Product = product;
+            Count = count;
+        }
+    }
+}
