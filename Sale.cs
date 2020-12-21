7691f215a8a2b5379ff5d752352f2974864528c9
VugarGuliyev <vugarmg@code.edu.az>
2020-12-21 10:50:07 +0400

    Sale Class Updated

--- Sale.cs
+++ Sale.cs
@@ -10,15 +10,14 @@ namespace MarketApp
     {
         private static int _id = 0;
         public readonly int ID;
-        public readonly DateTime Time;
-        public double TotalAmount;
+        public DateTime SaleDate { get; set; }
+        public double TotalAmount { get; set; }
 
         List<SaleItem> saleItems = new List<SaleItem>();
 
         public Sale()
         {
             ID = ++_id;
-            Time = DateTime.Now;
             TotalAmount = 0;
         }
     }
