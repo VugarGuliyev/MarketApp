d259a84bfedaf5af9814840e12e7c92f9ddc7ffc
VugarGuliyev <vugarmg@code.edu.az>
2020-12-21 11:19:16 +0400

    IMarketable Interface Created

---
+++ IMarketable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp
+{
+    interface IMarketable
+    {
+        void AddNewProduct();
+        void ChangeProductDetails();
+        void RemoveProduct();
+        void ShowAllProducts();
+        void ShowCategoryBasedProducts();
+        void ShowPriceBasedProducts();
+        void ShowNameBasedProducts();
+    }
+}
